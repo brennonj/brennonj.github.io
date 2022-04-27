d57df730135ccc82a0a2c1e426d97d8478caf010
Brennon Jacobson <brennonj@me.com>
2022-04-26 23:16:28 -0700

    update landing page content and styling

--- scripts/root-main.js
+++ scripts/root-main.js
@@ -1,11 +1,3 @@
-// toggle menu in small view
-const toggleMenu = () => {
-    document.querySelector('#menu').classList.toggle('open');
-}
-
-document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
-
-
 // set copy right to current year
 const currentYear = new Date().getFullYear();
 const yearDisplay = document.querySelector('#year');
