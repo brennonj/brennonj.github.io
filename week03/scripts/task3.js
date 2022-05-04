14bb84fce0678e03c807cb0b02bbfccdd4b931c0
Brennon Jacobson <brennonj@me.com>
2022-05-03 22:43:44 -0700

    Complete tasks 1-6

--- week03/scripts/task3.js
+++ week03/scripts/task3.js
@@ -3,16 +3,34 @@
 /* FUNCTIONS */
 
 // Step 1: Using function declaration, define a function named add that takes two arguments, number1 and number2
-
 // Step 2: In the function, return the sum of the parameters number1 and number2
+function add(number1, number2){
+    return number1 + number2;
+}
 
 // Step 3: Step 3: Using function declaration, define another function named addNumbers that gets the values of two HTML form controls with IDs of addend1 and addend2. Pass them to the add function
-
 // Step 4: Assign the return value to an HTML form element with an ID of sum
+function addNumbers(){
+    let addend1 = parseFloat(document.getElementById("addend1").value);
+    let addend2 = parseFloat(document.getElementById("addend2").value);
+    document.getElementById("sum").value = add(addend1, addend2);
+}
 
 // Step 5: Add a "click" event listener to the HTML button with an ID of addNumbers that calls the addNumbers function
+document.getElementById("addNumbers").addEventListener('click', addNumbers)
 
 // Step 6: Using function expressions, repeat Steps 1-5 with new functions named subtract and subtractNumbers and HTML form controls with IDs of minuend, subtrahend, difference and subtractNumbers
+function subtract(number1, number2){
+    return number1 - number2;
+}
+
+function subtractNumbers(){
+    let subtract_num1 = parseFloat(document.getElementById("minuend").value);
+    let subtract_num2 = parseFloat(document.getElementById("subtrahend").value);
+    document.getElementById("difference").value = subtract(subtract_num1, subtract_num2);
+}
+
+document.getElementById("subtractNumbers").addEventListener('click', subtractNumbers)
 
 // Step 7: Using arrow functions, repeat Steps 1-5 with new functions named multiply and mulitplyNumbers and HTML form controls with IDs of factor1, factor2, product and multiplyNumbers
 
