2d579bd7d5b4100437f68d34d3206a29d152ff5c
Brennon Jacobson <brennonj@me.com>
2022-05-03 23:08:47 -0700

    Implement Built In Methods

--- week03/scripts/task3.js
+++ week03/scripts/task3.js
@@ -64,13 +64,15 @@ document.getElementById("divideNumbers").addEventListener('click', divideNumbers
 /* BUILT-IN METHODS */
 
 // Step 1: Declare and instantiate a variable of type Date to hold the current date
+let myDate = new Date();
 
 // Step 2: Declare a variable to hold the current year
-
 // Step 3: Using the variable declared in Step 1, call the built-in getFullYear() method/function and assign it to the variable declared in Step 2
 
-// Step 4: Assign the current year variable to an HTML form element with an ID of year
+let currYear = myDate.getFullYear();
 
+// Step 4: Assign the current year variable to an HTML form element with an ID of year
+document.getElementById("year").textContent = currYear;
 
 /* ARRAY METHODS */
 
