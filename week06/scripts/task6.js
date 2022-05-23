973b002eeaa70cd6739212c913800aad22f1650b
Brennon Jacobson <brennonj@me.com>
2022-05-23 10:55:01 -0700

    Set up Inputs and JS file

---
+++ week06/scripts/task6.js
@@ -0,0 +1,17 @@
+// Get Password Length
+
+// Get if Special Characters
+
+// Get if Numbers
+
+// Get if Capital Letters
+
+
+
+// Generate Password
+
+
+// Display Password
+
+
+// Copy Password to Clipboard
