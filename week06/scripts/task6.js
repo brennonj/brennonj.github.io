768010db96a968ad355a3fc8e98bc716486d2dab
Brennon Jacobson <brennonj@me.com>
2022-05-23 22:48:20 -0700

    Make password generator

--- week06/scripts/task6.js
+++ week06/scripts/task6.js
@@ -1,17 +1,51 @@
-// Get Password Length
-
-// Get if Special Characters
-
-// Get if Numbers
-
-// Get if Capital Letters
-
-
 
 // Generate Password
+const generatePasswd = function(){
+    password = "";
+    // Get Password Length
+    let passwdLen = parseInt(document.getElementById("length").value);
+    // Get if Special Characters
+    let containSpecial = document.getElementById("special").checked;
+    // Get if Numbers
+    let containsNum = document.getElementById("numbers").checked;
+    // Get if Capital Letters
+    let containsCap = document.getElementById("capitals").checked;
 
+    chars = "abcdefghijklmnopqrstuvwxyz";
+    capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    numbers = "0123456789";
+    special = "!@#$%^&*()";
 
-// Display Password
+    if (containSpecial) {
+        console.log("Adding Special")
+        chars += special
+    };
+
+    if (containsCap) {
+        console.log("Adding Capitals")
+        chars += capitals
+    }
+
+    if (containsNum) {
+        console.log("Adding Numbers")
+        chars += numbers
+    }
+
+    for (var i = 0; i <= passwdLen; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber +1);
+    }
+
+    // Display Password
+    document.getElementById("passwd").value = password;
+};
+
+document.getElementById("generate").addEventListener('click', generatePasswd);
 
 
 // Copy Password to Clipboard
+const copyPassword = function(){
+    let passwdText = document.getElementById("passwd").value;
+};
+
+document.getElementById("copy").addEventListener('click', copyPassword);
